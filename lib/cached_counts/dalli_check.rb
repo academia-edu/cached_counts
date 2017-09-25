26723dfa2f0d67d9b292803318ccf3f3163f88f8
Richard Lee <dlackty@gmail.com>
2017-09-25 13:03:36 +0800

    Check Dalli defined

--- lib/cached_counts/dalli_check.rb
+++ lib/cached_counts/dalli_check.rb
@@ -1,6 +1,6 @@
 if defined?(Rails)
   ActiveSupport.on_load :cached_counts do
-    unless Rails.cache.respond_to?(:dalli) || Rails.cache.instance_variable_get(:@data).is_a?(Dalli::Client)
+    unless Rails.cache.respond_to?(:dalli) || (defined?(Dalli::Client) && Rails.cache.instance_variable_get(:@data).is_a?(Dalli::Client))
       raise "CachedCounts depends on Dalli!"
     end
   end
